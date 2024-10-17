1ba01291527157fe5e518b71bbb7b7f6fc8756b1
Opilane TTHK <opilane@tthk.ee>
2024-10-17 12:01:58 +0300

    Initial commit

---
+++ TannoMafia.Data/TannoMafiaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TannoMafia.Data
+{
+    public class TannoMafiaContext
+    {
+        public DbSet<Tanno> Tannos { get; set; }
+    }
+}
---
+++ tannomafia/Models/Tanno/TannoIndexViewModelcs.cs
@@ -0,0 +1,40 @@
+﻿namespace tannomafia.Models.Tannos
+{
+
+    public enum TitanType
+    {
+        Happy, Sad, Angry, High, Hungry, Drunk
+    }
+public enum TitanStatus
+    {
+        Dead, Alive,High, Drinking, Eating, Smoking
+    }
+
+
+
+
+
+    public class TannoIndexViewModelcs
+    {
+        public Guid ID { get; set; }
+        public string TitanName { get; set; }
+        public int TitanHealth { get; set; }
+        public int TitanXP { get; set; }
+        public int TitanXPNextLevel { get; set; }
+        public int TitanLevel { get; set; }
+        public TitanType TitanType { get; set; }
+        public TitanStatus TitanStatus { get; set; }
+        public int PrimaryAttackPower { get; set; }
+        public string PrimaryAttackName { get; set; }
+        public int SecondaryAttackPower { get; set; }
+        public string SecondaryAttackName { get; set; }
+        public int SpecialAttackPower { get; set; }
+        public string SpecialAttackName { get; set; }
+        public DateTime TitanWasBorn { get; set; }
+        public DateTime TitanDied { get; set; }
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
