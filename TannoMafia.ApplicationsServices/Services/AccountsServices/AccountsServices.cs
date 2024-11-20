e4f74bfe038d78e692922aa28f318e45c6cafda4
Opilane TTHK <opilane@tthk.ee>
2024-11-20 15:26:49 +0200

    interfa

--- TannoMafia.ApplicationsServices/Services/AccountsServices/AccountsServices.cs
+++ TannoMafia.ApplicationsServices/Services/AccountsServices/AccountsServices.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TannoMafia.Core.Domain;
+using TannoMafia.Core.ServicesInterFace;
 
 namespace TannoMafia.ApplicationsServices.Services.AccountsServices
 {
-    public class AccountsServices : IAccountServices
+    public class AccountsServices : IAccountsServices
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _SignInManager;
